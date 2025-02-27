cb682bf4d855fee0a6465ac27b54b902fc5b77cb
Constantin Bilz <cbilz@posteo.net>
2025-02-27 20:43:28 +0100

    Add `Diagnostic.appendExpectedAndActualLines`

--- src/Diagnostic.zig
+++ src/Diagnostic.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const assert = std.debug.assert;
 const Allocator = std.mem.Allocator;
 
 contents: std.ArrayListUnmanaged(u8),
@@ -62,4 +63,49 @@ pub fn append(
         bytes[i..],
         if (options.visible_end_of_text) "␃\n" else "", // End of Text symbol (ETX)
     });
+}
+
+/// Appends a diagnostic message comparing the `actual` string to one or more
+/// `expected_alternatives`. Asserts that `actual` differs from each of the alternatives.
+fn appendExpectedAndActualLines(
+    d: *Diagnostic,
+    allocator: Allocator,
+    expected_alternatives: []const []const u8,
+    actual: []const u8,
+) void {
+    // The implementation of this function was adapted from `std.testing.expectEqualStrings`.
+    assert(expected_alternatives.len != 0);
+
+    d.append(allocator, "============ found this: =============\n", .{});
+    d.append(allocator, actual, .{ .visible_newlines = true, .visible_end_of_text = true });
+
+    for (expected_alternatives, 0..) |expected, i| {
+        if (i == 0) {
+            d.append(allocator, "=========== expected this: ===========\n", .{});
+        } else {
+            d.append(allocator, "============== or this: ==============\n", .{});
+        }
+
+        d.append(allocator, expected, .{ .visible_newlines = true, .visible_end_of_text = true });
+
+        {
+            d.append(allocator, "======================================\n", .{});
+        }
+
+        const shortest = @min(expected.len, actual.len);
+        var line: usize = 1;
+        var column: usize = 1;
+        for (expected[0..shortest], actual[0..shortest]) |expected_byte, actual_byte| {
+            if (expected_byte != actual_byte) {
+                break;
+            } else if (actual_byte == '\n') {
+                line += 1;
+                column = 1;
+            } else {
+                column += 1;
+            }
+        } else assert(expected.len != actual.len);
+
+        d.print("\nFirst difference occurs on line {d}, column {d}.\n\n", .{ line, column });
+    }
 }
